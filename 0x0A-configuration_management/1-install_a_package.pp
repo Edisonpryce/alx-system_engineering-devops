750b597fc9868e94e09bc8e19f700bf9d03f0f96
Edison Asare <104757179+Edisonpryce@users.noreply.github.com>
2024-06-30 20:36:43 +0000

    Update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,18 @@
-# A manifest to install flask framwork via pip3 package management
+# A manifest to install flask framwork on Werzbeug and python3.8.1 via pip3 package management
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'python3.8.10':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
