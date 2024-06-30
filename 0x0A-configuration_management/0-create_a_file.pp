f8bb4f8ed19144fa15c79c98d99dcf5225ee508c
Edisonpryce <Edisonpryce@gmail.com>
2024-06-29 19:43:31 -0700

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,8 +0,0 @@
-# This manifest of the first task creates a file at /tmp/school...
-file { '/tmp/school':
-  ensure  => 'file',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-}
