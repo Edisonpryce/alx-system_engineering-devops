6aa529a3df2f282b4ae65321f64f3339312ac4af
Edison Asare <104757179+Edisonpryce@users.noreply.github.com>
2024-06-30 20:40:53 +0000

    Update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,13 +2,11 @@
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
 
 package { 'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
 
 package { 'python3.8.10':
